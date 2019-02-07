39914a8a089a4d5835a5e8998a2a7c2cdf906e39
Matteo Contrini <matteo@contrini.it>
2019-02-07 19:06:50 +0100

    add some keyboards

--- MarconiBot/Application.cs
+++ MarconiBot/Application.cs
@@ -1,18 +1,32 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace MarconiBot
 {
     class Application : IApplication
     {
         private ITelegramBotClient bot;
+        private ReplyKeyboardMarkup defaultKeyboard;
 
         public void Start()
         {
             this.bot = new TelegramBotClient("");
 
+            this.defaultKeyboard = new ReplyKeyboardMarkup(new List<List<KeyboardButton>>
+            {
+                new List<KeyboardButton>
+                {
+                    new KeyboardButton("5"),
+                    new KeyboardButton("10"),
+                }
+            }, true);
+
             this.bot.OnMessage += Bot_OnMessage;
+            this.bot.OnCallbackQuery += Bot_OnCallbackQuery;
             this.bot.StartReceiving();
         }
 
@@ -23,7 +37,11 @@ namespace MarconiBot
 
             if (inputText == "/start")
             {
-                await this.bot.SendTextMessageAsync(chatId, "Ciao! Per iniziare, inviami il valore che vuoi convertire");
+                await this.bot.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "Ciao! Per iniziare, inviami il valore che vuoi convertire",
+                    replyMarkup: defaultKeyboard
+                );
             }
             else
             {
@@ -47,13 +65,55 @@ namespace MarconiBot
 
                     string msg = $"{source}: {input}\n{destination}: {converted}";
 
-                    await this.bot.SendTextMessageAsync(chatId, msg);
+                    await this.bot.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: msg,
+                        replyMarkup: GenerateKeyboard(input)
+                    );
                 }
                 else
                 {
-                    await this.bot.SendTextMessageAsync(chatId, "Non ho capito cosa vuoi dire 🤔");
+                    await this.bot.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: "Non ho capito cosa vuoi dire 🤔",
+                        replyMarkup: defaultKeyboard
+                    );
                 }
             }
+        }
+
+        private InlineKeyboardMarkup GenerateKeyboard(double value)
+        {
+            return new InlineKeyboardMarkup(new List<List<InlineKeyboardButton>>
+                {
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData("🇪🇺 => 🇺🇸", $"EUR;USD;{value}"),
+                        InlineKeyboardButton.WithCallbackData("🇺🇸 => 🇪🇺", $"USD;EUR;{value}")
+                    }
+                }
+            );
+        }
+
+        private async void Bot_OnCallbackQuery(object sender, CallbackQueryEventArgs e)
+        {
+            string[] input = e.CallbackQuery.Data.Split(';');
+
+            Enum.TryParse(input[0], out Currency source);
+            Enum.TryParse(input[1], out Currency destination);
+            double value = double.Parse(input[2]);
+
+            CurrencyConverter converter = new CurrencyConverter();
+            double converted = converter.Convert(source, destination, value);
+
+            string msg = $"{source}: {value}\n{destination}: {converted}";
+
+            await this.bot.EditMessageTextAsync(
+                chatId: e.CallbackQuery.Message.Chat.Id,
+                messageId: e.CallbackQuery.Message.MessageId,
+                text: msg,
+                replyMarkup: GenerateKeyboard(value)
+            );
         }
     }
 }
